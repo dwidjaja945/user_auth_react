93a9d9a8044b4966c82d25df17b2549263e261ce
Dylan Widjaja <dwidjaja945@gmail.com>
2018-05-18 15:31:07 -0700

    added a navbar to navigate through the page

---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default (props) => (
+    <div>
+        <h1 className="center">this is the home page</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis eaque optio rerum! Odit nesciunt minus reprehenderit ullam. Impedit, ea libero!</p>
+    </div>
+)
---
+++ src/components/secret_doc.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (props) => (
+    <div>
+        <h1 className="center">Top Secret Documnt</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis eaque optio rerum! Odit nesciunt minus reprehenderit ullam. Impedit, ea libero!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, repellendus! Aliquid incidunt, sed dolorum unde possimus fugiat sunt consectetur eum ipsam, accusantium soluta natus.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi unde similique dolores, porro fugit officia itaque vel sed minus tempora distinctio exercitationem, nostrum minima ea ullam non eius, nemo explicabo obcaecati at voluptatum nam odio necessitatibus! Deleniti repellat quaerat molestiae iusto nihil consectetur nisi recusandae quod, eius quis provident voluptatibus.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet labore aliquam provident commodi officiis possimus, nemo fugit earum esse adipisci, ducimus natus illum eum, iste maxime. Laudantium, nulla! Beatae excepturi corrupti distinctio!</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, repudiandae.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, quibusdam hic dolorum minus perspiciatis vitae recusandae ratione harum neque consectetur, architecto repellendus quae!</p>
+    </div>
+);
